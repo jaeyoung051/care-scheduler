596dc6888c3c65691154fca0376cc82721e380c8
Seo-051 <kingdom5437@naver.com>
2026-06-12 21:34:53 +0900

    chore: configure mariadb and swagger runtime

---
+++ src/main/java/com/homecare/HomeCareApplication.java
@@ -0,0 +1,12 @@
+package com.homecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeCareApplication.class, args);
+    }
+}
